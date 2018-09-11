7493937e21fe4a7428e740901f215eee47a983a5
Stewart Hughes <stewvhughes@gmail.com>
2018-09-11 18:05:14 -0400

    Init Commit
    -I input 2 basic autonomous and teleop files from previous years that
    should give a solid basis for the competition,
    -The autonomous features 2 potential driving methods along with a few
    helper methods: one based on the phone's internal time keeping system as
    the match goes on and one based on the traditional "wait" system that
    stalls actions in the loop function.
    -Teleop features a basec 4-wheel drive system along with a telemetry
    statement to ease testing. In between the two there is also a commented
    out holonomic drive train formula that will work for sideways strafing
    should we need to use that in this competition...

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseAutonomous.java
@@ -0,0 +1,104 @@
+//package com.qualcomm.ftcrobotcontroller.opmodes;
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.ftcrobotcontroller.R;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.DcMotorController;
+//import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous(name="ColorAutonomous", group="ColorAutonomous")  //AUTONOMOUS!
+
+public class ColorSensorCode_Autonomous extends OpMode {
+
+    DcMotor leftBack;
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor rightBack;
+
+    public void start() {
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        leftBack = hardwareMap.dcMotor.get("leftBack");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+        rightBack = hardwareMap.dcMotor.get("rightBack");
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void init(){
+    }
+
+    // Drive Methods //
+    public void drivePower(double power) { //traditional way of movement
+        leftFront.setPower(power / 100);
+        rightFront.setPower(power / 100);
+        leftBack.setPower(power / 100);
+        rightBack.setPower(power / 100);
+    }
+    public void driveForward(int pos1, int pos2, int pos3, int pos4, int newTime) {
+        while (runtime.seconds() <= (runtime.seconds() + newTime)) {
+            leftFront.setPower(pos1 / 10);
+            leftBack.setPower(pos2 / 10);
+            rightFront.setPower(pos3 / 10);
+            rightBack.setPower(pos4 / 10);
+        }
+        wait(5);
+        runtime.reset();
+    }
+    public void turnRight(pos1, pos2){
+        while (runtime.seconds() <= (runtime.seconds() + newTime)) {
+            leftFront.setPower(pos1 / 10);
+            leftBack.setPower(pos2 / 10);
+        }
+        wait(5);
+        runtime.reset();
+    }
+    public void turnLeft(pos3, pos4){
+        while (runtime.seconds() <= (runtime.seconds() + newTime)) {
+            rightFront.setPower(pos3 / 10);
+            rightBack.setPower(pos4 / 10);
+        }
+        wait(5);
+        runtime.reset();
+    }
+    //OTHER USEFULL METHODS
+    public void stopRobot() {
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+        wait(1);
+    }
+    public void wait(int time) {
+        try {
+            Thread.sleep(time * 100); //10 = 1sec
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //actual running code
+    public void loop(){
+        // TIME CHECK
+        if ( time < 1) return;
+
+        //DRIVE
+
+        stopRobot();
+    }
+
+}
---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
@@ -0,0 +1,127 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+/*
+	Holonomic concepts from:
+	http://www.vexforum.com/index.php/12370-holonomic-drives-2-0-a-video-tutorial-by-cody/0
+   Robot wheel mapping:
+          X FRONT X
+        X           X
+      X  FL       FR  X
+              X
+             XXX
+              X
+      X  BL       BR  X
+        X           X
+          X       X
+*/
+@TeleOp(name = "Full Tele Op", group = "Tele Op")
+public class full_tele_op extends OpMode {
+
+    DcMotor motorFrontRight;
+    DcMotor motorFrontLeft;
+    DcMotor motorBackRight;
+    DcMotor motorBackLeft;
+
+//    double changeFactor = .9;
+//    double turnChangeFactor = .9;
+
+    @Override
+    public void init()
+    {
+        motorFrontRight = hardwareMap.dcMotor.get("front right");
+        motorFrontLeft = hardwareMap.dcMotor.get("front left");
+        motorBackLeft = hardwareMap.dcMotor.get("back left");
+        motorBackRight = hardwareMap.dcMotor.get("back right");
+    }
+    @Override
+    public void loop() {
+
+        //basic controls being write to robot
+        motorFrontRight = gamepad1.right_stick_y;
+        motorBackRight = gamepad1.right_stick_y;
+        motorFrontLeft = gamepad1.left_stick_y;
+        motorBackLeft = gamepad1.left_stick_y;
+
+//        float gamepad1LeftY = -gamepad1.left_stick_y;
+//        float gamepad1LeftX = gamepad1.left_stick_x;
+//        float gamepad1RightX = gamepad1.right_stick_x;
+//        float gamepad2LeftY = gamepad2.left_stick_y;
+//        float gamepad2RightY = gamepad2.right_stick_y;
+//        // holonomic formulas
+//        float FrontLeft = (float)((-gamepad1LeftY - gamepad1LeftX - gamepad1RightX * turnChangeFactor/*trying to decrease impact of turning*/) * changeFactor); // 9/10ths of the power
+//        float FrontRight = (float)((gamepad1LeftY - gamepad1LeftX - gamepad1RightX * turnChangeFactor) * changeFactor);
+//        float BackRight = (float)((gamepad1LeftY + gamepad1LeftX - gamepad1RightX * turnChangeFactor) * changeFactor);
+//        float BackLeft = (float)((-gamepad1LeftY + gamepad1LeftX - gamepad1RightX * turnChangeFactor) * changeFactor);
+//        if(gamepad1.left_stick_button || gamepad1.right_stick_button) { //scaling power of motors
+//            if(changeFactor == .9) {
+//                changeFactor = .5;
+//                turnChangeFactor = .5;
+//            }
+//            else {
+//                changeFactor = .9;
+//                turnChangeFactor = .9;
+//            }
+//        }
+//        // clip the right/left values so that the values never exceed +/- 1
+//        FrontRight = Range.clip(FrontRight, -1, 1);
+//        FrontLeft = Range.clip(FrontLeft, -1, 1);
+//        BackLeft = Range.clip(BackLeft, -1, 1);
+//        BackRight = Range.clip(BackRight, -1, 1);
+//        // write the values to the motors
+//        motorFrontRight.setPower(FrontRight);
+//        motorFrontLeft.setPower(FrontLeft);
+//        motorBackLeft.setPower(BackLeft);
+//        motorBackRight.setPower(BackRight);
+
+        telemetry.addData("Text", "*** Robot Data***");
+        telemetry.addData("Joy XL YL XR",  String.format("%.2f", gamepad1LeftX) + " " +
+                String.format("%.2f", gamepad1LeftY) + " " +  String.format("%.2f", gamepad1RightX));
+        telemetry.addData("f left pwr",  "front left  pwr: " + String.format("%.2f", FrontLeft));
+        telemetry.addData("f right pwr", "front right pwr: " + String.format("%.2f", FrontRight));
+        telemetry.addData("b right pwr", "back right pwr: " + String.format("%.2f", BackRight));
+        telemetry.addData("b left pwr", "back left pwr: " + String.format("%.2f", BackLeft));
+
+    }
+    @Override
+    public void stop() {
+    }
+    /*
+     * This method scales the joystick input so for low joystick values, the
+     * scaled value is less than linear.  This is to make it easier to drive
+     * the robot more precisely at slower speeds.
+     */
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
+    }
