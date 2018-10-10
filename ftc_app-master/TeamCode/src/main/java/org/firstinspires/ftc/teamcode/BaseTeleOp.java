bbda438ff14479a7f98c8c0adf583de3fa30ab09
Eleanor Mather <eleanor_mather@horacemann.org>
2018-10-10 15:54:54 -0400

    2 to 4 Motor Changes

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
@@ -32,154 +32,179 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name = "Full Tele Op", group = "Tele Op")
 public class full_tele_op extends OpMode {
 
-    DcMotor motorRight;
-    DcMotor motorLeft;
+  
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
     int driveSwitch = 1;
     DcMotor lift;
     DcMotor collect;
     DcMotor cExtend; //c stands for collect
     int collectSwitch = 1;
 
-//     @Override
+    //     @Override
     public void init()
     {
-        motorRight = hardwareMap.dcMotor.get("right motor");
-        motorLeft = hardwareMap.dcMotor.get("left motor");
-	lift = hardwareMap.dcMotor.get("lift");
-	collect = hardwareMap.dcMotor.get("collect");
-	cExtend = hardwareMap.dcMotor.get("cExtend");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        lift = hardwareMap.dcMotor.get("lift");
+        collect = hardwareMap.dcMotor.get("collect");
+        cExtend = hardwareMap.dcMotor.get("cExtend");
     }
-	
+
     public void loop() {
-	if(gamepad1.left_bumper){ //for drive train
-		if(gamepad1.dpad_up){
-		    driveSwitch = 1;
-		}
-		else if(gamepad1.dpad_right){
-		    driveSwitch = 2;
-		}
-		else if(gamepad1.dpad_left){
-		    driveSwitch = 3;
-		}
-		else if(gamepad1.dpad_down){
-		    driveSwitch = 4;
-		}
-		else{
-		}
-	}
-	if(gamepad1.right_bumpter){ //for collector
-		if(gamepad1.dpad_up){
-		    collectSwitch = 1;
-		}
-		else if(gamepad1.dpad_down){
-		    collectSwitch = 2;
-		}
-		else{
-		}
-	}
-	    
-	//controls that stay the same regardless of movement (can change to gamepad2 later)
-	if(gamepad1.x) { //lifter
-	    lift.setPower(1);
-	}
-	else if(gamepad1.y) {
-	    lift.setPower(-1);
-	}
-	else {
-	    lift.setPower(0);
-	}
-	if(gamepad1.a){
-	    collect.setPower(1);
-	}
-	else if(gamepad1.b){
-	    collect.setPower(-1);
-	}
-	else{
-	    collect.setPower(0);
-	}
-	    
+        if(gamepad1.left_bumper){ //for drive train
+            if(gamepad1.dpad_up){
+                driveSwitch = 1;
+            }
+            else if(gamepad1.dpad_right){
+                driveSwitch = 2;
+            }
+            else if(gamepad1.dpad_left){
+                driveSwitch = 3;
+            }
+            else if(gamepad1.dpad_down){
+                driveSwitch = 4;
+            }
+            else{
+            }
+        }
+        if(gamepad1.right_bumpter){ //for collector
+            if(gamepad1.dpad_up){
+                collectSwitch = 1;
+            }
+            else if(gamepad1.dpad_down){
+                collectSwitch = 2;
+            }
+            else{
+            }
+        }
+
+        //controls that stay the same regardless of movement (can change to gamepad2 later)
+        if(gamepad1.x) { //lifter
+            lift.setPower(1);
+        }
+        else if(gamepad1.y) {
+            lift.setPower(-1);
+        }
+        else {
+            lift.setPower(0);
+        }
+        if(gamepad1.a){
+            collect.setPower(1);
+        }
+        else if(gamepad1.b){
+            collect.setPower(-1);
+        }
+        else{
+            collect.setPower(0);
+        }
+
         //basic controls being write to robot (changing)
         switch (driveSwitch) {
             case 1: //basic joystick controls
-                motorRight = gamepad1.right_stick_y;
-                motorLeft = gamepad1.left_stick_y;
-		break;
+                backRight = gamepad1.right_stick_y;
+                frontRight = gamepad1.right_stick_y;
+                backLeft = gamepad1.left_stick_y;
+                frontLeft = gamepad1.right_stick_y;
+                
+                break;
             case 2: //full button controls
-		motorRight = gamepad1.x;
-                motorLeft = gamepad1.b;
+                backRight = gamepad1.x;
+                frontRight = gamepad1.x;
+                backLeft = gamepad1.b;
+                frontLeft = gamepad1.b;
+                
                 while (gamepad1.y){
-                    motorRight.setPower(5);
-                    motorLeft.setPower(5);
+                    frontRight.setPower(5);
+                    frontLeft.setPower(5);
+                    backRight.setPower(5);
+                    backLeft.setPower(5);
                 }
                 while (gamepad.a){
-                    motorRight.setPower(-5);
-                    motorLeft.setPower(-5);
+                    frontRight.setPower(-5);
+                    frontLeft.setPower(-5);
+                    backRight.setPower(-5);
+                    backLeft.setPower(-5);
                 }
-		break;
-	    case 3: //Moving with left, turning with right
-	        motorRight = gamepad1.left_stick_y;
-                motorLeft = gamepad1.left_stick_y;
-		if (gamepad1.right_stick_x > 0) {
-		    motorRight = -gamepad1.right_stick_x;
-		    motorLeft = gamepad1.right_stick_x;
-		}
-		else if(gamepad1.right_stick_x < 0) {
-		    motorRight = gamepad1.right_stick_x;
-		    motorLeft = -gamepad1.right_stick_x;
-		}
-		break;
-	    case 4: //turning with dPad, moving up/down with right stick
-		motorRight = gamepad1.right_stick_y;
-                motorLeft = gamepad1.right_stick_y;
+                break;
+            case 3: //Moving with left, turning with right
+                frontRight = gamepad1.left_stick_y;
+                frontLeft = gamepad1.left_stick_y;
+                backRight = gamepad1.left_stick_y;
+                backLeft = gamepad1.left_stick_y;
+                if (gamepad1.right_stick_x > 0) {
+                    frontRight = -gamepad1.right_stick_x;
+                    frontLeft = gamepad1.right_stick_x;
+                    backRight = -gamepad1.right_stick_x;
+                    backLeft = gamepad1.right_stick_x;
+                }
+                else if(gamepad1.right_stick_x < 0) {
+                    frontRight = gamepad1.right_stick_x;
+                    frontLeft = -gamepad1.right_stick_x;
+                    backRight = gamepad1.right_stick_x;
+                    backLeft = -gamepad1.right_stick_x;
+                }
+                break;
+            case 4: //turning with dPad, moving up/down with right stick
+                frontRight = gamepad1.right_stick_y;
+                frontLeft = gamepad1.right_stick_y;
+                backRight = gamepad1.right_stick_y;
+                backLeft = gamepad1.right_stick_y;
                 while(dpad_left){ //turn left
-                    motorRight.setPower(5);
+                    frontRight.setPower(5);
+                    backRight.setPower(5);
                 }
                 while(dpad_right){ //turn right
-                    motorLeft.setPower(5);
+                    frontLeft.setPower(5);
+                    backLeft.setPower(5);
                 }
-		break;
-	    default:
-		telemetry.addData("Text", "Drive Train Switch Error... fix me!!!!!"); //:)
-		break;
-	}
-	switch(collectSwitch){  //for collector stuff
-	    case 1:
-		cExtender = gamepad1.right_trigger;
-		cExtender = -gamepad1.left_trigger;
-		break;
-	    case 2:
-		boolean isExtended = false;
-		if(gamepad1.right_trigger > 0.1 && isExtended){
-		    cExtend.setPower(.75);
-		    wait(22);
-		    cExtend.setPower(-.2);
-		    wait(5);
-		    cExtend.setPower(0);
-		    isExtended = False;
-		}
-		else if(gamepad1.right_trigger > 0.1 && !isExtended){
-		    cExtend.setPower(.6);
-		    wait(30);
-		    cExtend.setPower(0);
-		    isExtended = True;
-		}
-		break;
-	    default:
-		telemetry.addData("Text", "Collector Switch Error... fix me!!!!!"); //:)
-		break;	
-	}
+                break;
+            default:
+                telemetry.addData("Text", "Drive Train Switch Error... fix me!!!!!"); //:)
+                break;
+        }
+        switch(collectSwitch){  //for collector stuff
+            case 1:
+                cExtender = gamepad1.right_trigger;
+                cExtender = -gamepad1.left_trigger;
+                break;
+            case 2:
+                boolean isExtended = false;
+                if(gamepad1.right_trigger > 0.1 && isExtended){
+                    cExtend.setPower(.75);
+                    wait(22);
+                    cExtend.setPower(-.2);
+                    wait(5);
+                    cExtend.setPower(0);
+                    isExtended = False;
+                }
+                else if(gamepad1.right_trigger > 0.1 && !isExtended){
+                    cExtend.setPower(.6);
+                    wait(30);
+                    cExtend.setPower(0);
+                    isExtended = True;
+                }
+                break;
+            default:
+                telemetry.addData("Text", "Collector Switch Error... fix me!!!!!"); //:)
+                break;
+        }
 
         telemetry.addData("Text", "*** Robot Data***");
-	telemetry.addData("Right Motor: " + motorRight); //drive
-	telemetry.addData("Left Motor: " + motorLeft);
+        telemetry.addData("Right Motor: " + motorRight); //drive
+        telemetry.addData("Left Motor: " + motorLeft);
     }
-//     @Override
+    //     @Override
     public void stop() {
-	motorRight.setPower(0);
-	motorLeft.setPower(0);
-	lift.setPower(0);
+        motorRight.setPower(0);
+        motorLeft.setPower(0);
+        lift.setPower(0);
     }
-	
+
     double scaleInput(double dVal)  { //extra scaling method
         double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
                 0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
