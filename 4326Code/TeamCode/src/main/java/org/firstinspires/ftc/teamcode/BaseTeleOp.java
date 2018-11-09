48b7578501617bb2e16f08983328b4c739be5baf
Shrey Sahgal <shreysahgal@gmail.com>
2018-11-09 16:46:32 -0500

    More Change

--- 4326Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
+++ 4326Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
@@ -1,109 +1,105 @@
+
 package org.firstinspires.ftc.teamcode;
 
-
-import com.qualcomm.ftcrobotcontroller.R;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
-import android.app.Activity;
-import android.graphics.Color;
-import android.view.View;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.DcMotorController;
-//import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.Servo;
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import java.lang.Runtime;
-import java.util.Timer;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.util.Range;
 
-/*
-          X FRONT X
-        X           X    <-- wheel spaces...?
-	    X FL     FR X
-             XXX
-              X
-      X  C        C  X <--castors
-              X
-             XXX
-              X
-      X  BL       BR  X
-        X           X
-          X       X
-	  Basic Controls:
-	  ~2 motor holonomic drive with varying methods using right/left stick controls
-	  ~x/y buttons controlling the potential lifer of the glyphs
-	  ~left/right trigger used for extending collecters
-	  ~b button used used for moving the collector
-	  ~left and right bumper used for switching drive train and collector mechanism controls
-*/
-@TeleOp(name = "BaseTeleOp", group = "BaseTeleOp")
+@TeleOp(name = "Base TeleOp", group = "Tele Op")
 public class BaseTeleOp extends OpMode {
 
 
-    DcMotor leftFront;
-    DcMotor rightFront;
-    DcMotor leftBack;
-    DcMotor rightBack;
-    DcMotor lift; //for outtake (though not sure how its going to work exactly)
-//    DcMotor flip;
-//    DcMotor collect;
-//    DcMotor cExtender; //c stands for collect --> aka intake
-//    int collectSwitch = 1;
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+    DcMotor lift;
+    Servo marker;
+    DcMotor flip;
+    CRServo intake;
 
-    //     @Override
+     @Override
     public void init()
     {
-        rightFront = hardwareMap.dcMotor.get("rightFront");
-        leftFront = hardwareMap.dcMotor.get("leftFront");
-        leftBack = hardwareMap.dcMotor.get("leftBack");
-        rightBack = hardwareMap.dcMotor.get("rightBack");
+        frontRight = hardwareMap.dcMotor.get("rightFront");
+        frontLeft = hardwareMap.dcMotor.get("leftFront");
+        backLeft = hardwareMap.dcMotor.get("leftBack");
+        backRight = hardwareMap.dcMotor.get("rightBack");
         lift = hardwareMap.dcMotor.get("lift");
-//        collect = hardwareMap.dcMotor.get("collect");
-//        cExtender = hardwareMap.dcMotor.get("cExtender");
-        leftBack.setDirection(DcMotor.Direction.REVERSE);
-        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        marker = hardwareMap.servo.get("marker");
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        flip = hardwareMap.dcMotor.get("flip");
+        intake = hardwareMap.crservo.get("intake");
     }
 
+    int driveSwitch = 1;
     public void loop() {
-        leftFront.setPower((float)(gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x));
-        rightFront.setPower((float)(-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x));
-        rightBack.setPower((float)(-gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x));
-        leftBack.setPower((float)(gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x));
+        if(gamepad1.left_bumper) { //for drive train
+            if (gamepad1.dpad_up) {
+                driveSwitch = 1;
+            }
+            if (gamepad1.dpad_up) {
+                driveSwitch = 2;
+            }
+            else{
+                telemetry.addData("text", "driveSwitch error...");
+            }
+        }
+         // for marker mech
 
-        if (gamepad1.right_bumper) {
-            lift.setPower(1);
-        } else if (gamepad1.left_bumper) {
-            lift.setPower(-1);
+        // for mecanum driving
+        if (driveSwitch == 1) {
+            frontLeft.setPower(gamepad1.right_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x); //right stick movement, left stick turning
+            frontRight.setPower(gamepad1.right_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x);
+            backLeft.setPower(gamepad1.right_stick_y - gamepad1.right_stick_x + gamepad1.left_stick_x);
+            backRight.setPower(gamepad1.right_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x);
+        }
+        else if (driveSwitch == 2){
+            float drive = gamepad1.right_stick_y;
+            float strafe = gamepad1.right_stick_x;
+            float turn = gamepad1.left_stick_x;
+
+            float fl = drive - strafe + turn;
+            float fr = drive + strafe - turn;
+            float bl = drive + strafe + turn;
+            float br = drive - strafe - turn;
+
+            frontLeft.setPower(fl);
+            frontRight.setPower(fr);
+            backLeft.setPower(bl);
+            backRight.setPower(br);
+        }
+        // for lifting
+        lift.setPower(gamepad2.right_stick_y);
+
+        // for flipping
+        flip.setPower(gamepad2.left_stick_y);
+        flip.setPower(gamepad2.left_stick_x / 4); // for testing flipper
+
+        // for intake crservo
+        if (gamepad2.right_trigger > 0) {
+            intake.setPower(scaleInput(gamepad2.left_trigger));
+        } else if (gamepad2.left_trigger > 0) {
+            intake.setPower(-scaleInput(gamepad2.left_trigger));
         } else {
-            lift.setPower(0);
+            intake.setPower(0);
         }
 
-        telemetry.addData("Text", "*** Robot Data***");
-        telemetry.addData("text", "Front Right: " + rightFront);
-        telemetry.addData("text", "Front Left: " + leftFront);
-        telemetry.addData("text", "Back Right: " + rightBack);
-        telemetry.addData("text", "Back Left: " + leftBack);
     }
-    //     @Override
-    public void wait(int time) { //10th seconds
-        try {
-            Thread.sleep(time * 100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+
     public void stop() {
-        rightFront.setPower(0);
-        leftFront.setPower(0);
-        leftBack.setPower(0);
-        rightBack.setPower(0);
+        frontRight.setPower(0);
+        frontLeft.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
         lift.setPower(0);
-
+        marker.setPosition(0);
+        intake.setPower(0);
     }
 
     double scaleInput(double dVal)  { //extra scaling method
