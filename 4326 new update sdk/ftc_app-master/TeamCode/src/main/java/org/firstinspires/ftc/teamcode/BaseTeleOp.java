a4b3689f59003a7f0d55d73840970f21d9d4e8b1
Stewart Hughes <stewvhughes@gmail.com>
2018-12-05 18:54:55 -0500

    Post Testing-Gamepad Changes
    -added marker functionality to gamepad1
    -swaped hinge/intake stuff to gamepad2 only

--- 4326 new update sdk/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
+++ 4326 new update sdk/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
@@ -18,9 +18,10 @@ public class BaseTeleOp extends OpMode {
     DcMotor backLeft;
     DcMotor backRight;
     DcMotor lift;
-    Servo marker;
-    DcMotor flip;
-    CRServo intake;
+    Servo   marker;
+    DcMotor flip; //arm
+    DcMotor box; //wrist
+    CRServo intake; //spiiiiiiin
 
      @Override
     public void init()
@@ -30,15 +31,20 @@ public class BaseTeleOp extends OpMode {
         backLeft = hardwareMap.dcMotor.get("leftBack");
         backRight = hardwareMap.dcMotor.get("rightBack");
         lift = hardwareMap.dcMotor.get("lift");
+        box = hardwareMap.dcMotor.get("box");
         marker = hardwareMap.servo.get("marker");
-        frontRight.setDirection(DcMotor.Direction.REVERSE);
-        backRight.setDirection(DcMotor.Direction.REVERSE);
         flip = hardwareMap.dcMotor.get("flip");
         intake = hardwareMap.crservo.get("intake");
+
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
     }
 
     int driveSwitch = 1;
     public void loop() {
+        if(gamepad2.y) {
+            telemetry.addData("text", "it works, its just aut  :/");
+        }
         if(gamepad1.left_bumper) { //for drive train
             if (gamepad1.dpad_up) {
                 driveSwitch = 1;
@@ -51,13 +57,21 @@ public class BaseTeleOp extends OpMode {
             }
         }
          // for marker mech
+        if(gamepad1.a){   //just in case
+            if(gamepad1.left_bumper){
+                marker.setPosition(0);
+            }
+            else if(gamepad1.right_bumper){
+                marker.setPosition(1);
+            }
+        }
 
         // for mecanum driving
         if (driveSwitch == 1) {
-            frontLeft.setPower(gamepad1.right_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x); //right stick movement, left stick turning
-            frontRight.setPower(gamepad1.right_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x);
-            backLeft.setPower(gamepad1.right_stick_y - gamepad1.right_stick_x + gamepad1.left_stick_x);
-            backRight.setPower(gamepad1.right_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x);
+            frontLeft.setPower(-gamepad1.right_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x); //right stick movement, left stick turning
+            frontRight.setPower(-gamepad1.right_stick_y - gamepad1.right_stick_x + gamepad1.left_stick_x);
+            backLeft.setPower(-gamepad1.right_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x);
+            backRight.setPower(-gamepad1.right_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x);
         }
         else if (driveSwitch == 2){
             float drive = gamepad1.right_stick_y;
@@ -77,17 +91,35 @@ public class BaseTeleOp extends OpMode {
         // for lifting
         lift.setPower(gamepad2.right_stick_y);
 
-        // for flipping
-        flip.setPower(gamepad2.left_stick_y);
-        flip.setPower(gamepad2.left_stick_x / 4); // for testing flipper
 
-        // for intake crservo
-        if (gamepad2.right_trigger > 0) {
-            intake.setPower(scaleInput(gamepad2.left_trigger));
-        } else if (gamepad2.left_trigger > 0) {
-            intake.setPower(-scaleInput(gamepad2.left_trigger));
-        } else {
+        // for flipping
+        flip.setPower(scaleInput(-gamepad2.left_stick_y)); //must be scaled
+
+        if(gamepad2.a){
+            intake.setPower(1);
+        }
+        else if(gamepad2.b){
+            intake.setPower(-1);
+        }
+        else{
             intake.setPower(0);
+        }
+//        if (gamepad2.right_trigger > 0) {
+//            intake.setPower(scaleInput(-gamepad2.right_trigger)); //intake
+//        } else if (gamepad2.left_trigger > 0) {
+//            intake.setPower(scaleInput(gamepad2.left_trigger)); //outtake
+//        } else {
+//            intake.setPower(0);
+//        }
+
+        //flipping the box
+        if(gamepad1.right_trigger > 0){
+            box.setPower(gamepad2.right_trigger/2);//used to be /1
+        }else if(gamepad1.left_trigger > 0) {
+            box.setPower(-gamepad2.left_trigger/2);
+
+        }else {
+            box.setPower(0);
         }
 
     }
@@ -99,7 +131,7 @@ public class BaseTeleOp extends OpMode {
         backRight.setPower(0);
         lift.setPower(0);
         marker.setPosition(0);
-        intake.setPower(0);
+       // intake.setPower(0);
     }
 
     double scaleInput(double dVal)  { //extra scaling method
